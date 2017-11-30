373cbfe972958ce42f1b64ecea683b14d21ec138
Caleb Blackwell <calebxblackwell@gmail.com>
2017-11-30 11:50:03 -0800

    third commit

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
+//youtube api request
+  function loadVideo(){
+    $.getJSON("https://www.googleapis.com/youtube/v3/search",
+    {
+      "q":searchTerm,
+      "part":'snippet',
+      "key":'AIzaSyDnFkudd6I1TMdIrE3OjM7wnIUsaMcRBPs',
+    });
+  }
+//instagram api request
+  function linkInstagram(){
+    $.getJSON("",
+    {
+      "q":searchTerm,
+      "part":'snippet',
+      "key":'', //https://www.instagram.com/developer/authentication/
+    });
+  }
+//the idea behind the instagram section is that you can click on popular viner's instagram profiles to follow them on there. Though depending on authentication, it could just be bubble images of their profiles with their username hyperlinked. 
+
+  
   
 });
