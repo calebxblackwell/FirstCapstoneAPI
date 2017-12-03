116f136a434fb4deba81e6fc5918dbf6b7de8599
Caleb Blackwell <calebxblackwell@gmail.com>
2017-12-02 17:20:43 -0800

    fifth commit

--- index.js
+++ index.js
@@ -19,6 +19,29 @@ $(document).ready(function(){
   }
 //the idea behind the instagram section is that you can click on popular viner's instagram profiles to follow them on there. Though depending on authentication, it could just be bubble images of their profiles with their username hyperlinked. 
 
+{
+
+  // Title is optional, but very helpful for later reference
+  "title": "Vine Rating Scale Survey"
+
+  // Owners email must match the account making calls to the API.
+  "owners": ["calebxblackwell@gmail.com"],
+  "wantedResponseCount": 100,
+  "audience": {
+    "country": "US",
+    "languages": ["en-US"]
+  },
+  
+"questions": [
+ {
+   "question": "Do you like Vine videos?",
+   "numStars":"ten",
+   "lowValueLabel": "Not much.",
+   "highValueLabel": "A lot!",
+   "type": "ratingScale"
+ }
+]
+}
   
   
 });
