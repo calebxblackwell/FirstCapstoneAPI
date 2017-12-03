f52db3f8569f311806d624dff66db1f4eff9bae7
Caleb Blackwell <calebxblackwell@gmail.com>
2017-12-03 10:33:19 -0800

    sixth commit

--- index.js
+++ index.js
@@ -1,47 +1,35 @@
-$(document).ready(function(){
-//youtube api request
-  function loadVideo(){
-    $.getJSON("https://www.googleapis.com/youtube/v3/search",
-    {
-      "q":searchTerm,
-      "part":'snippet',
-      "key":'AIzaSyDnFkudd6I1TMdIrE3OjM7wnIUsaMcRBPs',
-    });
-  }
-//instagram api request
-  function linkInstagram(){
-    $.getJSON("",
-    {
-      "q":searchTerm,
-      "part":'snippet',
-      "key":'', //https://www.instagram.com/developer/authentication/
-    });
-  }
-//the idea behind the instagram section is that you can click on popular viner's instagram profiles to follow them on there. Though depending on authentication, it could just be bubble images of their profiles with their username hyperlinked. 
-
-{
-
-  // Title is optional, but very helpful for later reference
-  "title": "Vine Rating Scale Survey"
-
-  // Owners email must match the account making calls to the API.
-  "owners": ["calebxblackwell@gmail.com"],
-  "wantedResponseCount": 100,
-  "audience": {
-    "country": "US",
-    "languages": ["en-US"]
-  },
-  
-"questions": [
- {
-   "question": "Do you like Vine videos?",
-   "numStars":"ten",
-   "lowValueLabel": "Not much.",
-   "highValueLabel": "A lot!",
-   "type": "ratingScale"
- }
-]
-}
-  
-  
+$(document).ready(function() {
+	//youtube api request
+	function loadVideo() {
+		$.getJSON("https://www.googleapis.com/youtube/v3/search", {
+			"q": searchTerm,
+			"part": 'snippet',
+			"key": 'AIzaSyDnFkudd6I1TMdIrE3OjM7wnIUsaMcRBPs',
+		});
+	}
+	//survey api request
+	function googleSurvey() {
+		$.getJSON("", {
+			"q": searchTerm,
+			"part": 'snippet',
+			"key": 'AIzaSyCsCqAZs51XlAKQm1hdne25TuPqy8AMB2s',
+		});
+	}
+	//below is random code from the survey google api site 
+	function surveyInfo(){
+		"title": "Vine Rating Scale Survey"
+		"owners": ["calebxblackwell@gmail.com"],
+		"wantedResponseCount": 100,
+		"audience": {
+			"country": "US",
+			"languages": ["en-US"]
+		},
+		"questions": [{
+			"question": "Do you like Vine videos?",
+			"numStars": "ten",
+			"lowValueLabel": "Not much.",
+			"highValueLabel": "A lot!",
+			"type": "ratingScale"
+		}]
+	}
 });
